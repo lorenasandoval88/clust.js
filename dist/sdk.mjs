98f903d333f0e3b8459024e1f1d0c36647db9cb7
lorenasandoval88 <lorenasandoval88@users.noreply.github.com>
2026-01-20 16:20:49 -0500

    Update sdk.mjs

---
+++ dist/sdk.mjs
@@ -0,0 +1,6 @@
+// sdk.mjs (built file — generated)
+export { pca, pca_UI } from "./pca.js";
+export { heatmap } from "./heatmap.js";
+export { hclust, hclust_plot } from "./hclust.js";
+
+export const version = "0.1.0";
